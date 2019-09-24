9cedcf8786b7dbe415d9eb0df182815eb1af1656
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2019-09-24 12:05:21 -0600

    updated cloud builders and seo stuff

--- builders/clasp/clasp_ci.sh
+++ builders/clasp/clasp_ci.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ ! -z "${_CLASPRC}" -a ! -f "${HOME}/.clasprc.json" ]; then
-echo $_CLASPRC > "$HOME/.clasprc.json"
+if [ ! -z "${CLASPRC}" -a ! -f "${HOME}/.clasprc.json" ]; then
+echo $CLASPRC > "${HOME}/.clasprc.json"
+echo "Created the clasprc"
 fi
 
-if [ ! -z "${_SCRIPT_ID}" -a ! -z "$PROJECT_ID" -a ! -f ".clasp.json" ]; then
+if [ ! -z "${SCRIPT_ID}" -a ! -z "$PROJECT_ID" -a ! -f ".clasp.json" ]; then
 cat > '.clasp.json' << EOF
-{"scriptId":"$_SCRIPT_ID","projectId": "$PROJECT_ID"}
+{"scriptId":"$SCRIPT_ID","projectId": "$PROJECT_ID"}
 EOF
+echo "created the clasp conf"
 fi
 
 clasp "$@"
