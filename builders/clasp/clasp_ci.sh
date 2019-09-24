36eb749a4bf4f3ebced0e6ab8b0db71d6526321e
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2019-09-24 12:49:28 -0600

    added rootDir to clasp build

--- builders/clasp/clasp_ci.sh
+++ builders/clasp/clasp_ci.sh
@@ -5,9 +5,9 @@ echo $CLASPRC > "${HOME}/.clasprc.json"
 echo "Created the clasprc"
 fi
 
-if [ ! -z "${SCRIPT_ID}" -a ! -z "$PROJECT_ID" -a ! -f ".clasp.json" ]; then
+if [ ! -z "${SCRIPT_ID}" -a ! -z "$PROJECT_ID" -a ! -z "$ROOT_DIR" -a ! -f ".clasp.json" ]; then
 cat > '.clasp.json' << EOF
-{"scriptId":"$SCRIPT_ID","projectId": "$PROJECT_ID"}
+{"scriptId":"$SCRIPT_ID","projectId": "$PROJECT_ID","rootDir":"$ROOT_DIR"}
 EOF
 echo "created the clasp conf"
 fi
